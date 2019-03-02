e00ec8d0684dd2d5effb00e70b122529c4101c30
Jose M Ramírez <47979556+jmrm1@users.noreply.github.com>
2019-03-02 16:05:08 +0100

    Create mod1_types_sentences.js
    Primer commit ejercicio 1

---
+++ Ejer1/mod1_types_sentences.js
@@ -0,0 +1,55 @@
+let hour = new Date().getHours();
+let greeting = "\n Good afternoon";
+if (hour >=7 && hour <=12){
+    greeting = "\n Good morning";
+}else if (hour >=13 && hour <=22){
+    greeting = "\n Good afternoon";
+}
+
+
+function tablaNumeros(fin)
+{
+   // console.log("\n***  La tabla con los números enteros de 0 a 22 en decimal, hexadecimal, octal y binario:");
+    
+    for(let i=0; i<= fin; i++)
+    {
+        console.log("\n"+ i + " dec = " + i.toString(10)+ " hex = " +i.toString(16)+" oct = "+i.toString(8)+" bin = "+i.toString(2));    
+    }
+}
+
+function tablaNumerosPares(inicio, fin)
+{
+    //console.log("\n***  Una tabla similar de 1 a 21, pero solo con los impares no incluidos en el intervalo de 10 a 20");
+    let resto;
+    for(let i=inicio; i<= fin; i++)
+    {
+        if (i <10 || i>20){
+            resto=(i%2);
+            //console.log("resto: "+ resto +"numero: "+ i);
+            if (resto!==0){ 
+                console.log("\n"+ i + " dec = " + i.toString(10)+ " hex = " +i.toString(16)+" oct = "+i.toString(8)+" bin = "+i.toString(2)); 
+            }
+        }
+    }
+}
+
+
+console.log(`${greeting}, its ${hour} o’clock`);
+//console.log(greeting+", its "+hour +" o’clock");
+
+// muestra el valor de PI con 6 decimales
+console.log("\nNúmero PI con 6 decimales: "+ Math.PI.toFixed(6) );
+
+// muetra los numero 
+tablaNumeros(22);
+
+// muestra los numeros 
+tablaNumerosPares(1,21);
+
+// Hola en chino
+console.log("\nHi in Chinese is written as: " + String("\u55e8\uff0c\u4f60\u597d\u5417"));
+
+console.log("\nThe program has ﬁnished");
+
+
+
