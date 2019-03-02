72ae6136e47ba8847b81b5d52190f22fec9dfece
Jose M Ramírez <47979556+jmrm1@users.noreply.github.com>
2019-03-02 16:11:12 +0100

    Update mod1_types_sentences.js
    Actualiazación del ejercicio.

--- Ejer1/mod1_types_sentences.js
+++ Ejer1/mod1_types_sentences.js
@@ -1,11 +1,16 @@
-let hour = new Date().getHours();
-let greeting = "\n Good afternoon";
-if (hour >=7 && hour <=12){
-    greeting = "\n Good morning";
-}else if (hour >=13 && hour <=22){
-    greeting = "\n Good afternoon";
+function Saludo()
+{
+    let hour = new Date().getHours();
+    let greeting = "\n Good afternoon";
+    if (hour >=7 && hour <=12){
+        greeting = "\n Good morning";
+    }else if (hour >=13 && hour <=22){
+        greeting = "\n Good afternoon";
+    }
+    
+    console.log(`${greeting}, its ${hour} o’clock`);
+    //console.log(greeting+", its "+hour +" o’clock");
 }
-
 
 function tablaNumeros(fin)
 {
@@ -33,22 +38,22 @@ function tablaNumerosPares(inicio, fin)
     }
 }
 
-
-console.log(`${greeting}, its ${hour} o’clock`);
-//console.log(greeting+", its "+hour +" o’clock");
+// muestra el saludo.
+Saludo();
 
 // muestra el valor de PI con 6 decimales
 console.log("\nNúmero PI con 6 decimales: "+ Math.PI.toFixed(6) );
 
-// muetra los numero 
+// muetra los números enteros de 0 a 22 en decimal, hexadecimal, octal y binario
 tablaNumeros(22);
 
-// muestra los numeros 
+// muetra los números  de 1 a 21, pero solo con los impares no incluidos en el intervalo de 10 a 20    
 tablaNumerosPares(1,21);
 
-// Hola en chino
+// Escribe la siguiente frase utilizando caracteres escapados para representar hola en chino
 console.log("\nHi in Chinese is written as: " + String("\u55e8\uff0c\u4f60\u597d\u5417"));
 
+//Finalizar con la siguiente línea
 console.log("\nThe program has ﬁnished");
 
 
